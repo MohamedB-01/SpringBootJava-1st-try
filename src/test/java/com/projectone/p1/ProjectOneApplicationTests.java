9d89529c688cf936f7129f1baa6c039ac509e0b6
MohamedB <m.m.bassiouni@gmail.com>
2022-05-22 14:04:04 -0700

    added packages and properties

---
+++ src/test/java/com/projectone/p1/ProjectOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectone.p1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
