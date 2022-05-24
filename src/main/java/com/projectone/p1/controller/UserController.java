6c8079b66ca252a4e1deb8d5bceb8b5c175ccf85
MohamedB <m.m.bassiouni@gmail.com>
2022-05-24 14:56:07 -0700

    added adduser, deleteuser, updateuser to usercontroller

--- src/main/java/com/projectone/p1/controller/UserController.java
+++ src/main/java/com/projectone/p1/controller/UserController.java
@@ -26,7 +26,62 @@ public class UserController {
     // register users
     @PostMapping
     public ResponseEntity<String> saveUser(@RequestBody User user){
+        ResponseEntity responseEntity = null;
+        if (userService.isUserExists(user.getUserId())) {
+            responseEntity = new ResponseEntity<String>
+                    ("user already exists", HttpStatus.CONFLICT);   //409
+        }else {
+            boolean result = userService.addUser(user);
+            if (result) {
+                responseEntity = new ResponseEntity<String>
+                        ("Successfully Saved User " + user.getUsername(), HttpStatus.OK);        //200
+            } else {
+                responseEntity = new ResponseEntity<String>
+                        ("Cannot save User", HttpStatus.NOT_ACCEPTABLE);        //406
+            }
 
+        }
+        return responseEntity;
+    }
+
+    //deleting users
+    @DeleteMapping("{userId}")
+    public ResponseEntity<User> deleteUser(@PathVariable("userId") int userId){
+        ResponseEntity responseEntity = null;
+        if (userService.isUserExists(userId)){
+            boolean result = userService.deleteUser(userId);
+            if (result) {
+                responseEntity = new ResponseEntity<String>
+                        ("Successfully deleted user " , HttpStatus.OK);        //200
+            } else {
+                responseEntity = new ResponseEntity<String>
+                        ("user was not deleted ", HttpStatus.EXPECTATION_FAILED);        //406
+            }
+        } else {
+            responseEntity = new ResponseEntity<String>
+                    ("User does not exist ", HttpStatus.NOT_FOUND);
+        }
+        return responseEntity;
+    }
+
+    // updating user
+    @PutMapping("{userId}")
+    public ResponseEntity<User> updateUser(@RequestBody User user){
+        ResponseEntity responseEntity = null;
+        if (userService.isUserExists(user.getUserId())){
+            boolean result = userService.updateUser(user);
+            if (result) {
+                responseEntity = new ResponseEntity<String>
+                        ("Successfully updated user " + user.getUsername(), HttpStatus.OK);        //200
+            } else {
+                responseEntity = new ResponseEntity<String>
+                        ("User records was not updated ", HttpStatus.EXPECTATION_FAILED);        //406
+            }
+        }else {
+            responseEntity = new ResponseEntity<String>
+                    ("User does not exist ", HttpStatus.NOT_FOUND);
+        }
+        return responseEntity;
     }
 
 
